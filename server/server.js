6c1a930f338233f33f21dccc66d674a8d635790f
unknown <rayner_tan@rocketmail.com>
2022-12-15 17:44:55 +0800

    Project Setup

---
+++ server/server.js
@@ -0,0 +1,10 @@
+//SERVER DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
